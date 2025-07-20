6d15450f1ca5b1dd1437d2e0229fbf829684d53f
Archer <josemiguelarcher@gmail.com>
2025-07-20 21:08:37 +0100

    Initial skeleton

---
+++ templates/foundation.php
@@ -0,0 +1,36 @@
+<?php function drawHeader()
+{ ?>
+    <!DOCTYPE html>
+    <html lang="pt">
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="assets/logo.png" type="image/png" />
+        <link rel="stylesheet" href="stylesheets/style.css">
+        <title>José Archer</title>
+    </head>
+
+    <body>
+        <header>
+            <img src="assets/logo.png" alt="logo">
+            <h1>Hello World</h1>
+        </header>
+        <main>
+        <?php } ?>
+
+        <?php function drawFooter()
+        { ?>
+        </main>
+        <footer>
+            <nav>
+                <a href="#">Instagram</a>
+                <a href="#">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">Github</a>
+            </nav>
+            <p>&copy; 2025 José Archer. All rights deserved.</p>
+        </footer>
+    </body>
+
+    </html>
+<?php } ?>

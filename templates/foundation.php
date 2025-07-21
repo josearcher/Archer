642fcf57745e9f6f932454d4addaa5394764a454
Archer <josemiguelarcher@gmail.com>
2025-07-21 23:20:37 +0100

    Somethings added in the main page

--- templates/foundation.php
+++ templates/foundation.php
@@ -7,7 +7,13 @@
         <meta charset="utf-8">
         <link rel="icon" href="assets/logo.png" type="image/png" />
         <link rel="stylesheet" href="stylesheets/style.css">
+        <link rel="stylesheet" href="stylesheets/layout.css">
+        <link rel="stylesheet" href="stylesheets/vars.css">
         <title>José Archer</title>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Denk+One&family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+        </style>
+        <script src="https://kit.fontawesome.com/1353c44cf3.js" crossorigin="anonymous" defer></script>
     </head>
 
     <body>
@@ -22,13 +28,13 @@
         { ?>
         </main>
         <footer>
+             <p>&copy; 2025 José Archer.</p>
             <nav>
-                <a href="#">Instagram</a>
-                <a href="#">Twitter</a>
-                <a href="#">Facebook</a>
-                <a href="#">Github</a>
+                <a href="https://www.instagram.com/josearcher12/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://x.com/JosArcher3"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.facebook.com/jose.archer.779"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://github.com/josearcher"><i class="fa-brands fa-github"></i></a>
             </nav>
-            <p>&copy; 2025 José Archer. All rights deserved.</p>
         </footer>
     </body>
 
